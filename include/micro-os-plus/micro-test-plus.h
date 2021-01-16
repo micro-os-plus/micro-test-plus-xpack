ff9e11584eaf877b5a8c0ce92002b443cb18fd1a
Liviu Ionescu <ilg@livius.net>
2021-01-17 00:06:31 +0200

    add first implementation

---
+++ include/micro-os-plus/micro-test-plus.h
@@ -0,0 +1,142 @@
+/*
+ * This file is part of the µOS++ distribution.
+ *   (https://github.com/micro-os-plus)
+ * Copyright (c) 2021 Liviu Ionescu.
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom
+ * the Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#ifndef MICRO_TEST_PLUS_MICRO_TEST_PLUS_H_
+#define MICRO_TEST_PLUS_MICRO_TEST_PLUS_H_
+
+// ----------------------------------------------------------------------------
+
+#ifdef __cplusplus
+
+namespace os
+{
+  namespace mtp // `micro-test-plus` is shortened to `mtp`.
+  {
+    class test
+    {
+    public:
+      test ();
+
+      test (int argc, char* argv[]);
+
+      test (const test&) = delete;
+      test (test&&) = delete;
+      test&
+      operator= (const test&)
+          = delete;
+      test&
+      operator= (test&&)
+          = delete;
+
+      ~test () = default;
+
+      void
+      init (int argc, char* argv[]);
+
+      void
+      start_suite (const char* name);
+
+      void
+      run_test_case (void (*f) (test&), const char* name);
+
+      void
+      pass (const char* message, const char* file = nullptr, int line = 0);
+
+      void
+      fail (const char* message, const char* file = nullptr, int line = 0);
+
+      void
+      expect_true (bool condition, const char* message,
+                   const char* file = nullptr, int line = 0);
+
+      void
+      expect_eq (int actual, int expected, const char* message,
+                 const char* file = nullptr, int line = 0);
+
+      void
+      expect_eq (long actual, long expected, const char* message,
+                 const char* file = nullptr, int line = 0);
+
+      void
+      expect_eq (const char* actual, const char* expected, const char* message,
+                 const char* file = nullptr, int line = 0);
+
+      int
+      result (void);
+
+      inline int
+      passed (void)
+      {
+        return passed_;
+      };
+
+      inline int
+      failed (void)
+      {
+        return failed_;
+      };
+
+      inline int
+      test_cases (void)
+      {
+        return test_cases_;
+      };
+
+    protected:
+      int argc;
+      char** argv;
+
+      const char* name_;
+
+      unsigned int passed_;
+      unsigned int failed_;
+      unsigned int test_cases_;
+    };
+  } // namespace mtp
+} // namespace os
+
+// ----------------------------------------------------------------------------
+
+// Macros are only a convenience to pass the file name and line number,
+// otherwise the direct methods can be used as well.
+#define MTP_EXPECT_EQ(t, actual, expected, message) \
+  t.expect_eq (actual, expected, message, __FILE__, __LINE__)
+#define MTP_EXPECT_TRUE(t, condition, message) \
+  t.expect_true (condition, message, __FILE__, __LINE__)
+
+#define MTP_PASS(t, message) t.pass (message, __FILE__, __LINE__)
+#define MTP_FAIL(t, message) t.fail (message, __FILE__, __LINE__)
+
+// ----------------------------------------------------------------------------
+
+#endif // __cplusplus
+
+// ----------------------------------------------------------------------------
+
+#endif // MICRO_TEST_PLUS_MICRO_TEST_PLUS_H_
+
+// ----------------------------------------------------------------------------
---
+++ src/micro-test-plus.cpp
@@ -0,0 +1,255 @@
+/*
+ * This file is part of the µOS++ distribution.
+ *   (https://github.com/micro-os-plus)
+ * Copyright (c) 2021 Liviu Ionescu.
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom
+ * the Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#include <micro-os-plus/micro-test-plus.h>
+#include <stdio.h>
+#include <string>
+
+// ----------------------------------------------------------------------------
+
+namespace os
+{
+  namespace mtp
+  {
+
+    // ------------------------------------------------------------------------
+
+    test::test ()
+    {
+      this->argc = 0;
+      this->argv = nullptr;
+
+      passed_ = 0;
+      failed_ = 0;
+      test_cases_ = 0;
+    }
+
+    test::test (int argc, char* argv[])
+    {
+      init (argc, argv);
+    }
+
+    void
+    test::init (int argc, char* argv[])
+    {
+      this->argc = argc;
+      this->argv = argv;
+
+      passed_ = 0;
+      failed_ = 0;
+      test_cases_ = 0;
+
+#if defined(__clang__)
+      printf ("Built with clang " __VERSION__);
+#else
+      printf ("Built with GCC " __VERSION__);
+#endif
+#if defined(__EXCEPTIONS)
+      printf (", with exceptions");
+#else
+      printf (", no exceptions");
+#endif
+#if defined(DEBUG)
+      printf (", with DEBUG");
+#endif
+      puts (".");
+
+#if defined(DEBUG)
+      printf ("argv[] = ");
+      for (int i = 0; i < argc; ++i)
+        {
+          printf ("'%s' ", argv[i]);
+        }
+      puts ("");
+#endif
+    }
+
+    void
+    test::start_suite (const char* name)
+    {
+      name_ = name;
+      printf ("\n%s started\n", name);
+    }
+
+    void
+    test::run_test_case (void (*func) (test&), const char* name)
+    {
+      printf ("\n  %s\n", name);
+
+      (*func) (*this);
+      test_cases_++;
+    }
+
+    void
+    test::pass (const char* message, const char* file __attribute__ ((unused)),
+                int line __attribute__ ((unused)))
+    {
+      // The file name and line number are unused in this version;
+      // they are present only in case future versions will keep a
+      // log off all tests.
+      printf ("    ✓ %s\n", message);
+      passed_++;
+    }
+
+    void
+    test::fail (const char* message, const char* file, int line)
+    {
+      if (file != nullptr)
+        {
+          printf ("    ✗ %s (in '%s:%d')\n", message, file, line);
+        }
+      else
+        {
+          printf ("    ✗ %s\n", message);
+        }
+
+      failed_++;
+    }
+
+    void
+    test::expect_true (bool condition, const char* message, const char* file,
+                       int line)
+    {
+      if (condition)
+        {
+          printf ("    ✓ %s\n", message);
+          passed_++;
+        }
+      else
+        {
+          if (file != nullptr)
+            {
+              printf ("    ✗ %s (in '%s:%d')\n", message, file, line);
+            }
+          else
+            {
+              printf ("    ✗ %s\n", message);
+            }
+
+          failed_++;
+        }
+    }
+
+    void
+    test::expect_eq (int actual, int expected, const char* message,
+                     const char* file, int line)
+    {
+      if (actual == expected)
+        {
+          printf ("    ✓ %s\n", message);
+          passed_++;
+        }
+      else
+        {
+          if (file != nullptr)
+            {
+              printf ("    ✗ %s (expected %d, got %d, in '%s:%d')\n", message,
+                      expected, actual, file, line);
+            }
+          else
+            {
+              printf ("    ✗ %s (expected %d, got %d)\n", message, expected,
+                      actual);
+            }
+          failed_++;
+        }
+    }
+
+    void
+    test::expect_eq (long actual, long expected, const char* message,
+                     const char* file, int line)
+    {
+      if (actual == expected)
+        {
+          printf ("    ✓ %s\n", message);
+          passed_++;
+        }
+      else
+        {
+          if (file != nullptr)
+            {
+              printf ("    ✗ %s (expected %ld, got %ld, in '%s:%d')\n",
+                      message, expected, actual, file, line);
+            }
+          else
+            {
+              printf ("    ✗ %s (expected %ld, got %ld)\n", message, expected,
+                      actual);
+            }
+          failed_++;
+        }
+    }
+
+    void
+    test::expect_eq (const char* actual, const char* expected,
+                     const char* message, const char* file, int line)
+    {
+      if (std::strcmp (actual, expected) == 0)
+        {
+          printf ("    ✓ %s\n", message);
+          passed_++;
+        }
+      else
+        {
+          if (file != nullptr)
+            {
+              printf ("    ✗ %s (expected %s, got %s, in '%s:%d')\n", message,
+                      expected, actual, file, line);
+            }
+          else
+            {
+              printf ("    ✗ %s (expected %s, got %s)\n", message, expected,
+                      actual);
+            }
+          failed_++;
+        }
+    }
+
+    int
+    test::result (void)
+    {
+      // Also fail if none passed.
+      if (failed_ == 0 && passed_ != 0)
+        {
+          printf ("\n%s passed (%d tests in %d test cases)\n", name_, passed_,
+                  test_cases_);
+          return 0;
+        }
+      else
+        {
+          printf (
+              "\n%s failed (%d tests passed, %d failed, in %d test cases)\n",
+              name_, passed_, failed_, test_cases_);
+          return 1;
+        }
+    }
+
+    // ------------------------------------------------------------------------
+  } // namespace mtp
+} // namespace os
+
+// ----------------------------------------------------------------------------
