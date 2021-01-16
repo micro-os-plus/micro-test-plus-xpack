41d7c48cb0dfc7741ce5d52579e5f0f74ec06cf7
Liviu Ionescu <ilg@livius.net>
2021-01-17 00:08:35 +0200

    add local tests

---
+++ tests/main.cpp
@@ -0,0 +1,157 @@
+/*
+ * This file is part of the µOS++ distribution.
+ *   (https://github.com/micro-os-plus)
+ * Copyright (c) 2021 Liviu Ionescu.
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom
+ * the Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#include <micro-os-plus/micro-test-plus.h>
+
+#include <cassert>
+#include <stdio.h>
+
+using namespace os;
+
+// ----------------------------------------------------------------------------
+
+// Each test case exercises a method or a family of methods.
+// After each test case, the caller checks the counts of
+// passed/failed test cases.
+
+void
+test_case_pass (mtp::test& t)
+{
+  t.pass ("it passed");
+  MTP_PASS (t, "it passed with macro");
+}
+
+void
+test_case_fail (mtp::test& t)
+{
+  t.fail ("it failed");
+  MTP_FAIL (t, "it failed with macro");
+}
+
+void
+test_case_expect_true (mtp::test& t)
+{
+  t.expect_true (true, "true passed");
+  t.expect_true (false, "false failed");
+  MTP_EXPECT_TRUE (t, false, "false with macro failed");
+}
+
+void
+test_case_expect_eq (mtp::test& t)
+{
+  t.expect_eq (1, 1, "1 == 1");
+  t.expect_eq (2, 1, "2 != 1");
+  MTP_EXPECT_EQ (t, 2, 1, "2 != 1 with macro");
+
+  t.expect_eq (1L, 1L, "1L == 1L");
+  t.expect_eq (2L, 1L, "2L != 1L");
+  MTP_EXPECT_EQ (t, 2L, 1L, "2L != 1L with macro");
+}
+
+void
+test_case_expect_eq_strings (mtp::test& t)
+{
+  t.expect_eq ("aaa", "aaa", "'aaa' == 'aaa'");
+  t.expect_eq ("aab", "aaa", "'aab' != 'aaa'");
+  MTP_EXPECT_EQ (t, "aac", "aaa", "'aac' != 'aaa'");
+
+  char* str = (char*)"aaa";
+  t.expect_eq (str, "aaa", "str == 'aaa' with aaa");
+  str = (char*)"aac";
+  t.expect_eq (str, "aaa", "str == 'aaa' with aac");
+  MTP_EXPECT_EQ (t, str, "aaa", "str == 'aaa' with aac");
+}
+
+// ----------------------------------------------------------------------------
+
+int
+main (int argc, char* argv[])
+{
+  mtp::test t (argc, argv);
+
+  printf ("\nµTest++ self test started\n");
+
+  t.start_suite ("Inner test");
+
+  assert (t.passed () == 0);
+  assert (t.failed () == 0);
+  assert (t.test_cases () == 0);
+
+  // --------------------------------------------------------------------------
+
+  t.run_test_case (test_case_pass, "Check if t.pass() always succeeds");
+
+  assert (t.passed () == 2);
+  assert (t.failed () == 0);
+  assert (t.test_cases () == 1);
+
+  // --------------------------------------------------------------------------
+
+  t.run_test_case (test_case_fail, "Check if t.fail() always fails");
+
+  assert (t.passed () == 2);
+  assert (t.failed () == 2);
+  assert (t.test_cases () == 2);
+
+  // --------------------------------------------------------------------------
+
+  t.run_test_case (test_case_expect_true, "Check t.expect_true(condition)");
+
+  assert (t.passed () == 3);
+  assert (t.failed () == 4);
+  assert (t.test_cases () == 3);
+
+  // --------------------------------------------------------------------------
+
+  t.run_test_case (test_case_expect_eq,
+                   "Check t.expect_eq(actual, expected) with integers");
+
+  assert (t.passed () == 5);
+  assert (t.failed () == 8);
+  assert (t.test_cases () == 4);
+
+  // --------------------------------------------------------------------------
+
+  t.run_test_case (test_case_expect_eq_strings,
+                   "Check t.expect_eq(actual, expected) with strings");
+
+  assert (t.passed () == 7);
+  assert (t.failed () == 12);
+  assert (t.test_cases () == 5);
+
+  // --------------------------------------------------------------------------
+
+  // The inner test should return failure.
+  assert (t.result () != 0);
+
+  // On failure it aborts before reaching this point.
+  printf ("\nµTest++ self test passed\n");
+
+  return 0;
+}
+
+// ----------------------------------------------------------------------------
---
+++ tests/sample.cpp
@@ -0,0 +1,101 @@
+/*
+ * This file is part of the µOS++ distribution.
+ *   (https://github.com/micro-os-plus)
+ * Copyright (c) 2021 Liviu Ionescu.
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom
+ * the Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#include <micro-os-plus/micro-test-plus.h>
+
+using namespace os;
+
+// Forward definitions of the test cases.
+void
+test_case_something (mtp::test& t);
+
+void
+test_case_exception (mtp::test& t);
+
+// The test suite.
+int
+main (int argc, char* argv[])
+{
+  mtp::test t (argc, argv);
+
+  t.start_suite ("Sample test");
+
+  t.run_test_case (test_case_something, "Check various conditions");
+
+  t.run_test_case (test_case_exception, "Check if exceptions are thrown");
+
+  return t.result ();
+}
+
+// Simple examples of functions to be tested.
+int
+compute_one ()
+{
+  return 1;
+}
+
+char*
+compute_aaa ()
+{
+  return (char*)"aaa";
+}
+
+bool
+compute_condition ()
+{
+  return true;
+}
+
+// Test equality or logical conditions.
+void
+test_case_something (mtp::test& t)
+{
+  // Currently only int and long values can be compared.
+  // For everything else use casts.
+  MTP_EXPECT_EQ (t, compute_one (), 1, "compute_one() == 1");
+
+  // Strings can also be compared (via `strcmp()`).
+  MTP_EXPECT_EQ (t, compute_aaa (), "aaa", "compute_aaa() == 'aaa'");
+
+  // More complex conditions are passed as booleans.
+  MTP_EXPECT_TRUE (t, compute_condition (), "condition() is true");
+}
+
+// Test is something throws exceptions.
+void
+test_case_exception (mtp::test& t)
+{
+  try
+    {
+      // Do something that may throw
+      MTP_PASS (t, "exception not thrown");
+    }
+  catch (...)
+    {
+      MTP_FAIL (t, "exception thrown");
+    }
+}
