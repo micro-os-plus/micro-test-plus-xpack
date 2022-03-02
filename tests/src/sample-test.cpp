b9bdb0fff5333b2958990b3396e723c7111691ac
Liviu Ionescu <ilg@livius.net>
2022-03-02 23:10:31 +0200

    sample-test: rework with new comparators

--- tests/src/sample-test.cpp
+++ tests/src/sample-test.cpp
@@ -34,7 +34,23 @@ compute_one (void)
 static const char*
 compute_aaa (void)
 {
-  return "aaa";
+  // Construct it from parts, otherwise the compiler will coalesce strings
+  // and comparing addresses will match.
+  static char str[10];
+  strcpy (str, "aa");
+  strcat (str, "a");
+  return str;
+}
+
+static char*
+compute_aaaa (void)
+{
+  // Construct it from parts, otherwise the compiler will coalesce strings
+  // and comparing addresses will match.
+  static char str[10];
+  strcpy (str, "aa");
+  strcat (str, "aa");
+  return str;
 }
 
 static bool
@@ -61,16 +77,10 @@ exercise_throw (bool mustThrow)
 
 using namespace micro_test_plus;
 
-// clang-format off
 static test_suite ts = { "My test suite", [] {
-  test_case ("Check one", [] {
-    pass ("Passed");
-  });
-  test_case ("Check two", [] {
-    pass ("Passed");
-  });
-}};
-// clang-format on
+                          test_case ("Check one", [] { pass ("Passed"); });
+                          test_case ("Check two", [] { pass ("Passed"); });
+                        } };
 
 // ----------------------------------------------------------------------------
 
@@ -78,46 +88,49 @@ static test_suite ts = { "My test suite", [] {
 int
 main (int argc, char* argv[])
 {
-  micro_test_plus::init ("Sample Test", argc, argv);
+  micro_test_plus::initialize ("Sample Test", argc, argv);
 
   // --------------------------------------------------------------------------
 
   // Test equality or logical conditions.
-  micro_test_plus::test_case ("Check various conditions", [] {
+  test_case ("Check various conditions", [] {
+    pass ("Passed");
+    // fail("Failed");
+
     // Currently only int and long values can be compared.
     // For everything else use casts.
-    micro_test_plus::expect_equal (compute_one (), 1, "compute_one() == 1");
+    expect (eq (compute_one (), 1), "compute_one() == 1");
 
     // Strings can also be compared (via `strcmp()`).
-    micro_test_plus::expect_equal (compute_aaa (), "aaa",
-                                   "compute_aaa() == 'aaa'");
+    expect (eq (std::string_view{ compute_aaa () }, std::string_view{ "aaa" }),
+            "compute_aaa() == 'aaa' string_view");
+    expect (eq (compute_aaa (), "aaa"), "compute_aaa() == 'aaa' char*");
+    expect (eq (compute_aaaa (), "aaaa"), "compute_aaaa() == 'aaaa' char*");
 
     // More complex conditions are passed as booleans.
-    micro_test_plus::expect_true (compute_condition (), "condition() is true");
+    expect (compute_condition (), "condition() is true");
 
     auto f = [] (int i) { return i + 42; };
 
-    micro_test_plus::expect_equal (f (1), 43, "lambda == 43");
-
-    micro_test_plus::expect_true (f (2) == 44, "lambda == 44");
+    expect (eq (f (1), 43), "lambda == 43");
   });
 
   // --------------------------------------------------------------------------
 
   // Check main arguments.
-  micro_test_plus::test_case (
+  test_case (
       "Check args",
       [] (int argc, char* argv[]) {
-        micro_test_plus::expect_equal (argc, 3, "argc == 3");
+        expect (eq (argc, 3), "argc == 3");
 
         if (argc > 1)
           {
-            micro_test_plus::expect_equal (argv[1], "one", "argv[1] == 'one'");
+            expect (eq (argv[1], "one"), "argv[1] == 'one'");
           }
 
         if (argc > 2)
           {
-            micro_test_plus::expect_equal (argv[2], "two", "argv[2] == 'two'");
+            expect (eq (argv[2], "two"), "argv[2] == 'two'");
           }
       },
       argc, argv);
@@ -126,42 +139,26 @@ main (int argc, char* argv[])
 
 #if defined(__EXCEPTIONS)
 
-  micro_test_plus::test_case ("Check if exceptions are thrown", [] {
-    try
-      {
-        // Do something that throws.
-        exercise_throw (true);
-
-        // If we reached here, the exception was not thrown.
-        micro_test_plus::fail ("exception not thrown");
-      }
-    catch (...)
-      {
-        // Got it.
-        micro_test_plus::pass ("exception thrown");
-      }
+  test_case ("Check if exceptions are thrown", [] {
+    expect (throws ([] { exercise_throw (true); }), "exception thrown");
+    expect (
+        throws<std::runtime_error> ([] { throw std::runtime_error{ "" }; }),
+        "std::runtime_error thrown");
+#if 0
+    expect (throws<std::runtime_error> ([] { exercise_throw (true); }),
+            "std::runtime_error thrown");
+#endif
   });
 
-  micro_test_plus::test_case ("Check if exceptions are not thrown", [] {
-    try
-      {
-        // Do something that may throw, but it doesn'runner.
-        exercise_throw (false);
-
-        // If we reached here, everything is fine.
-        micro_test_plus::pass ("exception not thrown");
-      }
-    catch (...)
-      {
-        micro_test_plus::fail ("exception thrown");
-      }
+  test_case ("Check if exceptions are not thrown", [] {
+    expect (nothrow ([] { exercise_throw (false); }), "exception not thrown");
   });
 
 #endif // defined(__EXCEPTIONS)
 
   // --------------------------------------------------------------------------
 
-  return micro_test_plus::result ();
+  return micro_test_plus::exit_code ();
 }
 
 // ----------------------------------------------------------------------------
