037c767e0561147d1d33fd594c78d3656c7c7f9d
Liviu Ionescu <ilg@livius.net>
2022-03-14 10:11:21 +0200

    split test-suite.cpp

--- src/test-suite.cpp
+++ src/test-suite.cpp
@@ -21,75 +21,69 @@
 #endif // MICRO_OS_PLUS_INCLUDE_CONFIG_H
 
 #include <micro-os-plus/micro-test-plus.h>
-#include <cstring>
-// <iostream> is too heavy for embedded, use printf().
-#include <stdio.h>
-#include <unistd.h>
 
 // ----------------------------------------------------------------------------
 
-//#pragma GCC diagnostic ignored "-Waggregate-return"
 #if defined(__clang__)
 #pragma clang diagnostic ignored "-Wc++98-compat"
-#pragma clang diagnostic ignored "-Wc++98-c++11-c++14-compat"
+//#pragma clang diagnostic ignored "-Wc++98-compat-pedantic"
 #endif
 
 namespace micro_os_plus::micro_test_plus
 {
   // --------------------------------------------------------------------------
-  // Public API.
+
+  test_suite::test_suite (const char* name)
+  {
+#if defined(MICRO_OS_PLUS_TRACE_MICRO_TEST_PLUS)
+    micro_os_plus::trace::printf ("%s\n", __PRETTY_FUNCTION__);
+#endif // MICRO_OS_PLUS_TRACE_MICRO_TEST_PLUS
+
+    name_ = name;
+    // callable_ = [] {};
+
+    // The default test suite needs no registration.
+  }
 
   void
-  initialize (const char* name, int argc, char* argv[])
+  test_suite::run ()
   {
-#if defined(MICRO_TEST_PLUS_DEBUG)
-    trace::printf ("%s\n", __PRETTY_FUNCTION__);
-#endif
-    runner.initialize (name, argc, argv);
+    if (callable_ != nullptr)
+      {
+        begin ();
+        callable_ ();
+        end ();
+      }
   }
 
-  int
-  exit_code (void)
+  void
+  test_suite::begin (void)
   {
-    return runner.exit_code ();
+    current_test_suite = this;
+
+    reporter.begin_test_suite (name_);
   }
 
-  // --------------------------------------------------------------------------
-
-  // Too small to deserve a separate source file.
-  namespace reflection
+  void
+  test_suite::end (void)
   {
+    reporter.end_test_suite (*this);
+  }
 
-    const char*
-    short_name (const char* name)
-    {
-      const char* p = strrchr (name, '/');
-      if (p != nullptr)
-        return p + 1;
-      else
-        return name;
-    }
+  void
+  test_suite::begin_test_case (const char* name)
+  {
+    test_case_name_ = name;
+    ++test_cases_;
 
-  } // namespace reflection
+    reporter.begin_test_case (test_case_name_);
+  }
 
-  // ==========================================================================
-
-#if defined(__GNUC__)
-#pragma GCC diagnostic push
-#if defined(__clang__)
-#pragma clang diagnostic ignored "-Wglobal-constructors"
-#endif
-#endif
-
-  // Static instances;
-  test_runner runner;
-  test_reporter reporter;
-
-  test_suite* current_test_suite;
-
-#if defined(__GNUC__)
-#pragma GCC diagnostic pop
-#endif
+  void
+  test_suite::end_test_case (void)
+  {
+    reporter.end_test_case (test_case_name_);
+  }
 
   // --------------------------------------------------------------------------
 } // namespace micro_os_plus::micro_test_plus
