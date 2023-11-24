7a71772c838d538094954a5954c4c0deddf87042
Liviu Ionescu <ilg@livius.net>
2023-11-24 21:40:36 +0200

    add minimal-test.cpp for native builds

---
+++ tests/src/minimal-test.cpp
@@ -0,0 +1,87 @@
+/*
+ * This file is part of the µOS++ distribution.
+ *   (https://github.com/micro-os-plus/)
+ * Copyright (c) 2021 Liviu Ionescu.
+ *
+ * Permission to use, copy, modify, and/or distribute this software
+ * for any purpose is hereby granted, under the terms of the MIT license.
+ *
+ * If a copy of the license was not distributed with this file, it can
+ * be obtained from https://opensource.org/licenses/MIT/.
+ */
+
+// ----------------------------------------------------------------------------
+
+#if defined(MICRO_OS_PLUS_INCLUDE_CONFIG_H)
+#include <micro-os-plus/config.h>
+#endif // MICRO_OS_PLUS_INCLUDE_CONFIG_H
+
+#include <micro-os-plus/micro-test-plus.h>
+
+#include <string_view>
+#include <stdexcept>
+#include <vector>
+
+// ----------------------------------------------------------------------------
+
+using namespace std::literals;
+
+// ----------------------------------------------------------------------------
+
+#pragma GCC diagnostic ignored "-Waggregate-return"
+#if defined(__clang__)
+#pragma clang diagnostic ignored "-Wc++98-compat"
+#pragma clang diagnostic ignored "-Wshadow-uncaptured-local"
+#pragma clang diagnostic ignored "-Wexit-time-destructors"
+#pragma clang diagnostic ignored "-Wglobal-constructors"
+#pragma clang diagnostic ignored "-Wctad-maybe-unsupported"
+#pragma clang diagnostic ignored "-Wunknown-warning-option"
+#endif
+
+// ----------------------------------------------------------------------------
+
+// Simple examples of functions to be tested.
+static int
+compute_answer (void)
+{
+  // The Answer to the Ultimate Question of Life,
+  // the Universe and Everything is...
+  return 42;
+}
+
+static bool
+compute_condition (void)
+{
+  return true;
+}
+
+// ----------------------------------------------------------------------------
+
+int
+main (int argc, char* argv[])
+{
+  using namespace micro_os_plus::micro_test_plus;
+
+  // There is a default test suite automatically defined in main().
+  initialize (argc, argv, "Minimal");
+
+  // --------------------------------------------------------------------------
+
+  // Test comparison functions.
+  test_case ("Check various conditions", [] {
+    // There are functions with usual names for all comparisons.
+
+    expect (eq (compute_answer (), 42)) << "answer is 42";
+
+    // Boolean expressions can be checked directly.
+    expect (compute_condition ()) << "condition is true";
+  });
+
+  // --------------------------------------------------------------------------
+
+  // Trigger the execution of the separate test suites and
+  // return the overall test result to the system.
+  return exit_code ();
+}
+
+// ----------------------------------------------------------------------------
