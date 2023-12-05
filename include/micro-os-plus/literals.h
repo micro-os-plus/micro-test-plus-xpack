24606d7b7318e18fa5080c600b4c1e0b74f181f2
Liviu Ionescu <ilg@livius.net>
2023-12-06 00:16:18 +0200

    literals.h: add to_*, deprecate _*

--- include/micro-os-plus/literals.h
+++ include/micro-os-plus/literals.h
@@ -347,40 +347,55 @@ namespace micro_os_plus::micro_test_plus
 
   // --------------------------------------------------------------------------
 
-  /**
-   * @addtogroup micro-test-plus-literals
-   * @{
-   */
-
-  // Wrappers that can be used to convert dynamic values to specific types
-  // that are recognised by the comparators.
-  // The syntax is similar to function calls, like `_i(expression)`, but the
-  // results have custom types expected by comparators.
+  /** @deprecated Use `to_b` (since 3.2.0). */
   using _b = type_traits::value<bool>;
+  /** @deprecated Use `to_c` (since 3.2.0). */
   using _c = type_traits::value<char>;
+  /** @deprecated Use `to_sc` (since 3.2.0). */
   using _sc = type_traits::value<signed char>;
+  /** @deprecated Use `to_s` (since 3.2.0). */
   using _s = type_traits::value<short>;
+  /** @deprecated Use `to_i` (since 3.2.0). */
   using _i = type_traits::value<int>;
+  /** @deprecated Use `to_l` (since 3.2.0). */
   using _l = type_traits::value<long>;
+  /** @deprecated Use `to_ll` (since 3.2.0). */
   using _ll = type_traits::value<long long>;
+  /** @deprecated Use `to_u` (since 3.2.0). */
   using _u = type_traits::value<unsigned>;
+  /** @deprecated Use `to_uc` (since 3.2.0). */
   using _uc = type_traits::value<unsigned char>;
+  /** @deprecated Use `to_us` (since 3.2.0). */
   using _us = type_traits::value<unsigned short>;
+  /** @deprecated Use `to_ul` (since 3.2.0). */
   using _ul = type_traits::value<unsigned long>;
+  /** @deprecated Use `to_ull` (since 3.2.0). */
   using _ull = type_traits::value<unsigned long long>;
+  /** @deprecated Use `to_i8` (since 3.2.0). */
   using _i8 = type_traits::value<std::int8_t>;
+  /** @deprecated Use `to_i16` (since 3.2.0). */
   using _i16 = type_traits::value<std::int16_t>;
+  /** @deprecated Use `to_i32` (since 3.2.0). */
   using _i32 = type_traits::value<std::int32_t>;
+  /** @deprecated Use `to_i64` (since 3.2.0). */
   using _i64 = type_traits::value<std::int64_t>;
+  /** @deprecated Use `to_u8` (since 3.2.0). */
   using _u8 = type_traits::value<std::uint8_t>;
+  /** @deprecated Use `to_u16` (since 3.2.0). */
   using _u16 = type_traits::value<std::uint16_t>;
+  /** @deprecated Use `to_u32` (since 3.2.0). */
   using _u32 = type_traits::value<std::uint32_t>;
+  /** @deprecated Use `to_u64` (since 3.2.0). */
   using _u64 = type_traits::value<std::uint64_t>;
+  /** @deprecated Use `to_f` (since 3.2.0). */
   using _f = type_traits::value<float>;
+  /** @deprecated Use `to_d` (since 3.2.0). */
   using _d = type_traits::value<double>;
+  /** @deprecated Use `to_ld` (since 3.2.0). */
   using _ld = type_traits::value<long double>;
 
   /**
+   * @deprecated Use `to_t` (since 3.2.0).
    * @}
    */
 
@@ -392,6 +407,84 @@ namespace micro_os_plus::micro_test_plus
   struct _t : type_traits::value<T>
   {
     constexpr explicit _t (const T& t) : type_traits::value<T>{ t }
+    {
+    }
+  };
+
+
+  /**
+   * @addtogroup micro-test-plus-literals
+   * @{
+   */
+
+  // Wrappers that can be used to convert dynamic values to specific types
+  // that are recognised by the comparators.
+  // The syntax is similar to function calls, like `_i(expression)`, but the
+  // results have custom types expected by comparators.
+  /** @since 3.2.0 */
+  using to_b = type_traits::value<bool>;
+  /** @since 3.2.0 */
+  using to_c = type_traits::value<char>;
+  /** @since 3.2.0 */
+  using to_sc = type_traits::value<signed char>;
+  /** @since 3.2.0 */
+  using to_s = type_traits::value<short>;
+  /** @since 3.2.0 */
+  using to_i = type_traits::value<int>;
+  /** @since 3.2.0 */
+  using to_l = type_traits::value<long>;
+  /** @since 3.2.0 */
+  using to_ll = type_traits::value<long long>;
+  /** @since 3.2.0 */
+  using to_u = type_traits::value<unsigned>;
+  /** @since 3.2.0 */
+  using to_uc = type_traits::value<unsigned char>;
+  /** @since 3.2.0 */
+  using to_us = type_traits::value<unsigned short>;
+  /** @since 3.2.0 */
+  using to_ul = type_traits::value<unsigned long>;
+  /** @since 3.2.0 */
+  using to_ull = type_traits::value<unsigned long long>;
+  /** @since 3.2.0 */
+  using to_i8 = type_traits::value<std::int8_t>;
+  /** @since 3.2.0 */
+  using to_i16 = type_traits::value<std::int16_t>;
+  /** @since 3.2.0 */
+  using to_i32 = type_traits::value<std::int32_t>;
+  /** @since 3.2.0 */
+  using to_i64 = type_traits::value<std::int64_t>;
+  /** @since 3.2.0 */
+  using to_u8 = type_traits::value<std::uint8_t>;
+  /** @since 3.2.0 */
+  using to_u16 = type_traits::value<std::uint16_t>;
+  /** @since 3.2.0 */
+  using to_u32 = type_traits::value<std::uint32_t>;
+  /** @since 3.2.0 */
+  using to_u64 = type_traits::value<std::uint64_t>;
+  /** @since 3.2.0 */
+  using to_f = type_traits::value<float>;
+  /** @since 3.2.0 */
+  using to_d = type_traits::value<double>;
+  /** @since 3.2.0 */
+  using to_ld = type_traits::value<long double>;
+
+  /**
+   * @}
+   */
+
+  /**
+   * @ingroup micro-test-plus-literals
+   * @brief Template for wrapping any other type.
+   * @since 3.2.0
+   *
+   * @details
+   * A public class used only to convert the type.
+   */
+  template <class T>
+  struct to_t : type_traits::value<T>
+  {
+    /** @brief Constructor. */
+    constexpr explicit to_t (const T& t) : type_traits::value<T>{ t }
     {
     }
   };
