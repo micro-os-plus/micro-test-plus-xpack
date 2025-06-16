b8132edd6d6fb5cfaf10ca679905505bba3a4e9b
Liviu Ionescu <ilg@livius.net>
2025-06-16 15:40:19 +0300

    website doxygen update

--- website/doxygen/top-common.h
+++ website/doxygen/top-common.h
@@ -64,7 +64,6 @@ All files in this directory are intended for inclusion in user projects and prov
 @brief Topics related to the µOS++ framework.
 
 @details
-## Description
 
 The [µOS++ Framework](https://micro-os-plus.github.io) is the principal project
 within the µOS++ (micro-os-plus) ecosystem, providing a comprehensive suite of modern
