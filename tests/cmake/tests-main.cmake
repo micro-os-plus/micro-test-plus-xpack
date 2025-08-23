858b92d2c49a77eca1dc871b313504059819c0f3
Liviu Ionescu <ilg@livius.net>
2025-08-23 11:30:21 +0300

    move code from test-main.cmake to top

--- tests/cmake/tests-main.cmake
+++ tests/cmake/tests-main.cmake
@@ -14,20 +14,6 @@
 # -----------------------------------------------------------------------------
 
 # -----------------------------------------------------------------------------
-# Project wide setup #
-
-# Enable the languages used in the tests.
-enable_language(C)
-enable_language(CXX)
-enable_language(ASM)
-
-# Specify the C/C++ standards.
-set(CMAKE_C_STANDARD 11)
-set(CMAKE_C_STANDARD_REQUIRED ON)
-set(CMAKE_C_EXTENSIONS OFF)
-set(CMAKE_CXX_STANDARD 20)
-set(CMAKE_CXX_STANDARD_REQUIRED ON)
-set(CMAKE_CXX_EXTENSIONS OFF)
 
 # Generate the compile_commands.json file to feed the indexer.
 # Highly recommended, to help IDEs construct the index.
@@ -35,19 +21,6 @@ set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
 
 # Enable this to see the dependency graph.
 # set_property(GLOBAL PROPERTY GLOBAL_DEPENDS_DEBUG_MODE 1)
-
-# -----------------------------------------------------------------------------
-
-# Protect against in-source builds.
-if(CMAKE_BINARY_DIR STREQUAL CMAKE_SOURCE_DIR)
-  message(FATAL_ERROR "In-source builds are not supported. Please use a separate folder for build.")
-endif()
-
-# The build-helper is a devDependency, available only after `xpm install`.
-list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_SOURCE_DIR}/xpacks/@micro-os-plus/build-helper/cmake")
-include("micro-os-plus-build-helper")
-
-xpack_display_greetings("${CMAKE_CURRENT_SOURCE_DIR}/../package.json")
 
 # -----------------------------------------------------------------------------
 
@@ -70,12 +43,11 @@ endif()
 # common to all platforms.
 include("cmake/common-options.cmake")
 
+# Platform specific definitions.
+include("platforms/${PLATFORM_NAME}/cmake/definitions.cmake")
+
 # Set `xpack_dependencies_folders` with the platform specific dependencies.
 include("platforms/${PLATFORM_NAME}/cmake/dependencies-folders.cmake")
-
-# Define `micro-os-plus::platform` with the platform definitions,
-# common to all tests.
-include("platforms/${PLATFORM_NAME}/cmake/platform-options.cmake")
 
 # Iterate the platform dependencies and `add_subdirectory()`.
 xpack_add_dependencies_subdirectories("${xpack_dependencies_folders}" "xpacks-bin")
