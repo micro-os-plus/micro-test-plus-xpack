511f8cf4b4d66d702fa57ea76b1f0aefa69aa340
Liviu Ionescu <ilg@livius.net>
2026-03-23 23:43:41 +0200

    re-generate website commons

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -33,7 +33,7 @@ function HomepageHeader() {
         <p className="hero__subtitle">{siteConfig.tagline}
         </p>
         <div className={styles.installWithCopy}>
-          <InstallWithCopy>xpm install @micro-os-plus/micro-test-plus@3.3.0 --verbose</InstallWithCopy>
+          <InstallWithCopy>xpm install @micro-os-plus/micro-test-plus@3.3.1 --verbose</InstallWithCopy>
         </div>
       </div>
     </header>
