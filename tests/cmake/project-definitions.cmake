759051fe5d9f45c11be5f63f7a92db16ec6db3d5
Liviu Ionescu <ilg@livius.net>
2026-03-23 12:20:08 +0200

    move tests to sources

--- tests/cmake/project-definitions.cmake
+++ tests/cmake/project-definitions.cmake
@@ -23,19 +23,19 @@ set (xpack_dependencies_project_folders)
 # Each test has its own library. Add only those enabled.
 if (XPACK_ENABLE_MINIMAL_TEST)
   list (APPEND xpack_dependencies_project_folders
-        "${CMAKE_SOURCE_DIR}/minimal-test"
+        "${CMAKE_SOURCE_DIR}/sources/minimal-test"
   )
 endif ()
 
 if (XPACK_ENABLE_SAMPLE_TEST)
   list (APPEND xpack_dependencies_project_folders
-        "${CMAKE_SOURCE_DIR}/sample-test"
+        "${CMAKE_SOURCE_DIR}/sources/sample-test"
   )
 endif ()
 
 if (XPACK_ENABLE_UNIT_TEST)
   list (APPEND xpack_dependencies_project_folders
-        "${CMAKE_SOURCE_DIR}/unit-test"
+        "${CMAKE_SOURCE_DIR}/sources/unit-test"
   )
 endif ()
 
