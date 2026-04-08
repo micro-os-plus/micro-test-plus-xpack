c43dbffab1876ce0e4adb239ad4eb16636fc6907
Liviu Ionescu <ilg@livius.net>
2026-04-08 22:12:52 +0300

    #23: add test code to compare tap files

---
+++ tests/scripts/compare-tap-files.sh
@@ -0,0 +1,64 @@
+#!/usr/bin/env bash
+
+# -----------------------------------------------------------------------------
+#
+# This file is part of the µOS++ project (http://micro-os-plus.github.io).
+# Copyright (c) 2024-2026 Liviu Ionescu.  All rights reserved.
+#
+# Permission to use, copy, modify, and/or distribute this software
+# for any purpose is hereby granted, under the terms of the MIT license.
+#
+# If a copy of the license was not distributed with this file, it can
+# be obtained from https://opensource.org/licenses/mit.
+#
+# -----------------------------------------------------------------------------
+
+# -----------------------------------------------------------------------------
+# Safety settings (see https://gist.github.com/ilg-ul/383869cbb01f61a51c4d).
+
+if [[ ! -z ${DEBUG} ]]
+then
+  set ${DEBUG} # Activate the expand mode if DEBUG is anything but empty.
+else
+  DEBUG=""
+fi
+
+set -o errexit # Exit if command failed.
+set -o pipefail # Exit if pipe failed.
+set -o nounset # Exit if variable not set.
+
+# Remove the initial space and instead use '\n'.
+IFS=$'\n\t'
+
+# -----------------------------------------------------------------------------
+# Identify the script location, to reach, for example, the helper scripts.
+
+script_path="$0"
+if [[ "${script_path}" != /* ]]
+then
+  # Make relative path absolute.
+  script_path="$(pwd)/$0"
+fi
+
+export script_path
+export script_name="$(basename "${script_path}")"
+
+export script_folder_path="$(dirname "${script_path}")"
+export script_folder_name="$(basename "${script_folder_path}")"
+
+# =============================================================================
+
+if [ $# -lt 2 ]
+then
+  echo "Usage: compare-tap-files.sh <actual> <expected>"
+  exit 1
+fi
+
+# Strip the info lines and the line numbers from both files before comparing.
+diff -u \
+  <(grep -v -e '^# Running:' -e '^# Built with' -e '  line: ' -e '  condition: 0x' "$1") \
+  <(grep -v -e '^# Running:' -e '^# Built with' -e '  line: ' -e '  condition: 0x' "$2")
+
+echo "$(basename "$1") ok"
+
+# -----------------------------------------------------------------------------
