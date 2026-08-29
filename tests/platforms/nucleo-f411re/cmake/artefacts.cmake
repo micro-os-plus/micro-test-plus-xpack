e0558def3d37e297cfeea6c598e150167b62b220
Liviu Ionescu <ilg@livius.net>
2026-08-29 17:26:51 +0300

    platforms/nucleo-f411re update

---
+++ tests/platforms/nucleo-f411re/cmake/artefacts.cmake
@@ -0,0 +1,225 @@
+# -----------------------------------------------------------------------------
+# DO NOT EDIT! Automatically generated from template file:
+# build-helper/templates/common/_micro-os-plus/tests/platforms/qemu-cortex-m4f/CMakeLists-liquid.txt
+#
+# This file is part of the µOS++ project (https://micro-os-plus.github.io/).
+# Copyright (c) 2022-2026 Liviu Ionescu. All rights reserved.
+#
+# Permission to use, copy, modify, and/or distribute this software for any
+# purpose is hereby granted, under the terms of the MIT license.
+#
+# If a copy of the license was not distributed with this file, it can be
+# obtained from https://opensource.org/licenses/mit.
+#
+# -----------------------------------------------------------------------------
+
+# Create the platform specific test executables.
+
+# -----------------------------------------------------------------------------
+
+message (VERBOSE
+         "Including 'tests/platforms/${PLATFORM_NAME}/cmake/artefacts.cmake'..."
+)
+
+# -----------------------------------------------------------------------------
+
+# The libraries are compiled before the platform library, so they need to get
+# the same compile options.
+add_compile_common_private_options (
+  micro-os-plus-micro-test-plus-static-library
+)
+
+# -----------------------------------------------------------------------------
+
+set (XPACK_OPENOCD_INTERFACE "stlink-dap.cfg")
+set (XPACK_OPENOCD_TARGET "stm32f4x.cfg")
+
+function (add_openocd_test)
+
+  cmake_parse_arguments (PARSE_ARGV 0 arg "" "NAME" "COMMAND")
+
+  # First element is the ELF base name (argv[0] in the semihosting config).
+  list (GET arg_COMMAND 0 name)
+
+  set (semihosting_cmdline "arm semihosting_cmdline")
+  foreach (a IN LISTS arg_COMMAND)
+    string (APPEND semihosting_cmdline " ${a}")
+  endforeach ()
+
+  add_test (
+    NAME "${arg_NAME}"
+    COMMAND
+      openocd${extension}
+      # -d3
+      -c "gdb port disabled" -c "tcl port disabled" -c "telnet port disabled"
+      -f "interface/${XPACK_OPENOCD_INTERFACE}" -f
+      "target/${XPACK_OPENOCD_TARGET}" -c "program ${name}.elf verify" -c
+      "arm semihosting enable" -c "${semihosting_cmdline}" -c "reset"
+  )
+
+endfunction ()
+
+# -----------------------------------------------------------------------------
+
+if (XPACK_ENABLE_EMPTY_TEST)
+  add_cross_test_executable (empty-test)
+  target_link_cross_test_libraries (empty-test tests::empty)
+
+  xpack_display_target_lists (empty-test)
+  message (VERBOSE "A> empty-test")
+
+  add_openocd_test (
+    NAME
+    "empty-test"
+    COMMAND
+    empty-test
+    one
+    two
+    --output-file
+    empty-test.tap
+  )
+
+  add_compare_files_test (
+    NAME "empty-test-compare"
+    DEPENDS "empty-test"
+    FILES "${CMAKE_CURRENT_BINARY_DIR}/empty-test.tap"
+          "${CMAKE_CURRENT_SOURCE_DIR}/assets/tap/empty-test.tap"
+  )
+endif ()
+
+# -----------------------------------------------------------------------------
+
+if (XPACK_ENABLE_MINIMAL_TEST)
+  add_cross_test_executable (minimal-test)
+  target_link_cross_test_libraries (minimal-test tests::minimal)
+
+  xpack_display_target_lists (minimal-test)
+  message (VERBOSE "A> minimal-test")
+
+  add_openocd_test (
+    NAME
+    "minimal-test"
+    COMMAND
+    minimal-test
+    one
+    two
+    --output-file
+    minimal-test.tap
+  )
+
+  add_compare_files_test (
+    NAME "minimal-test-compare"
+    DEPENDS "minimal-test"
+    FILES "${CMAKE_CURRENT_BINARY_DIR}/minimal-test.tap"
+          "${CMAKE_CURRENT_SOURCE_DIR}/assets/tap/minimal-test.tap"
+  )
+endif ()
+
+# -----------------------------------------------------------------------------
+
+if (XPACK_ENABLE_SUITE_TEST)
+  add_cross_test_executable (suite-test)
+  target_link_cross_test_libraries (suite-test tests::suite)
+
+  xpack_display_target_lists (suite-test)
+  message (VERBOSE "A> suite-test")
+
+  add_openocd_test (
+    NAME
+    "suite-test"
+    COMMAND
+    suite-test
+    one
+    two
+    --output-file
+    suite-test.tap
+  )
+
+  add_compare_files_test (
+    NAME "suite-test-compare"
+    DEPENDS "suite-test"
+    FILES "${CMAKE_CURRENT_BINARY_DIR}/suite-test.tap"
+          "${CMAKE_CURRENT_SOURCE_DIR}/assets/tap/suite-test.tap"
+  )
+endif ()
+
+# -----------------------------------------------------------------------------
+
+if (XPACK_ENABLE_SAMPLE_TEST)
+  add_cross_test_executable (sample-test)
+  target_link_cross_test_libraries (sample-test tests::sample)
+
+  xpack_display_target_lists (sample-test)
+  message (VERBOSE "A> sample-test")
+
+  add_openocd_test (
+    NAME
+    "sample-test"
+    COMMAND
+    sample-test
+    one
+    two
+    --output-file
+    sample-test.tap
+  )
+
+  add_compare_files_test (
+    NAME "sample-test-compare"
+    DEPENDS "sample-test"
+    FILES "${CMAKE_CURRENT_BINARY_DIR}/sample-test.tap"
+          "${CMAKE_CURRENT_SOURCE_DIR}/assets/tap/sample-test.tap"
+  )
+endif ()
+
+# -----------------------------------------------------------------------------
+
+# if (XPACK_ENABLE_UNIT_TEST AND NOT CMAKE_BUILD_TYPE STREQUAL "Debug")
+if (XPACK_ENABLE_UNIT_TEST)
+  add_cross_test_executable (unit-test)
+  target_link_cross_test_libraries (unit-test tests::unit)
+
+  xpack_display_target_lists (unit-test)
+  message (VERBOSE "A> unit-test")
+
+  # The TAP output is too verbose and takes to long to be displayed by the
+  # OpenOCD semihosting.
+  if (false)
+    add_openocd_test (
+      NAME
+      "unit-test"
+      COMMAND
+      unit-test
+      one
+      two
+      --output-file
+      unit-test.tap
+    )
+    add_compare_files_test (
+      NAME "unit-test-compare"
+      DEPENDS "unit-test"
+      FILES "${CMAKE_CURRENT_BINARY_DIR}/unit-test.tap"
+            "${CMAKE_CURRENT_SOURCE_DIR}/assets/tap/unit-test.tap"
+    )
+  endif ()
+
+  add_openocd_test (
+    NAME
+    "unit-test-reporter-human"
+    COMMAND
+    unit-test
+    one
+    two
+    --reporter
+    human
+    --output-file
+    unit-test.txt
+  )
+  add_compare_files_test (
+    NAME "unit-test-reporter-human-compare"
+    DEPENDS "unit-test-reporter-human"
+    FILES "${CMAKE_CURRENT_BINARY_DIR}/unit-test.txt"
+          "${CMAKE_CURRENT_SOURCE_DIR}/assets/txt/unit-test.txt"
+  )
+endif ()
+
+# -----------------------------------------------------------------------------
---
+++ tests/platforms/nucleo-f411re/device/src/reset-handler.c
@@ -0,0 +1,80 @@
+/*
+ * This file is part of the µOS++ project (https://micro-os-plus.github.io/).
+ * Copyright (c) 2022-2026 Liviu Ionescu. All rights reserved.
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose is hereby granted, under the terms of the MIT license.
+ *
+ * If a copy of the license was not distributed with this file, it can be
+ * obtained from https://opensource.org/licenses/mit.
+ */
+
+// ----------------------------------------------------------------------------
+
+#include "micro-os-plus/device.h"
+
+#include <stdint.h>
+
+// ----------------------------------------------------------------------------
+
+typedef void
+(*handler_ptr_t)(void);
+
+extern handler_ptr_t _interrupt_vectors[];
+
+extern void __attribute__ ((noreturn, weak))
+_start (void);
+
+// ----------------------------------------------------------------------------
+
+// Begin address for the initialisation values of the .data section.
+extern uintptr_t __data_load_addr__;
+// Begin address for the .data section.
+extern uintptr_t __data_begin__;
+// End address for the .data section.
+extern uintptr_t __data_end__;
+
+extern uint32_t __stack;
+
+// ----------------------------------------------------------------------------
+
+void __attribute__ ((section (".after_vectors"), noreturn, naked))
+Reset_Handler (void);
+
+// QEMU sets SP to the first word in flash.
+void 
+Reset_Handler (void)
+{
+  // For just in case, when started via a debugger.
+  __asm__ (" MSR msp, %0 " : : "r"(&__stack) :);
+  // cortexm_architecture_set_msp(&__stack);
+
+  // SCB
+  // https://developer.arm.com/documentation/dui0552/a/cortex-m3-peripherals/system-control-block
+
+  // SCB->VTOR
+  // https://developer.arm.com/documentation/dui0552/a/cortex-m3-peripherals/system-control-block/vector-table-offset-register
+  // Mandatory when running from RAM. Not available on Cortex-M0.
+#if defined(__ARM_ARCH_7M__) || defined(__ARM_ARCH_7EM__) \
+    || defined(__ARM_ARCH_8M_MAIN__) || defined(__ARM_ARCH_8M_BASE__)
+  // *((uint32_t*)0xE000ED08)
+  SCB->VTOR = ((uint32_t)_interrupt_vectors & (uint32_t)(~0x3F));
+#endif // defined(__ARM_ARCH_7M__) ...
+
+  // Floating point instructions can be used early in the startup sequence
+  // as a result of compiler optimisations, therefore enable the FPU before 
+  // calling any functions. (`SystemInit()` happens too late).
+#if (defined (__FPU_USED) && (__FPU_USED == 1U)) || \
+    (defined (__ARM_FEATURE_MVE) && (__ARM_FEATURE_MVE > 0U))
+  // Enable CP10 and CP11 coprocessor.
+  SCB->CPACR |= ((3U << 10U*2U) |           /* enable CP10 Full Access */
+                 (3U << 11U*2U)  );         /* enable CP11 Full Access */
+
+  // Lazy save.
+  FPU->FPCCR |= FPU_FPCCR_ASPEN_Msk | FPU_FPCCR_LSPEN_Msk;
+#endif // defined (__FPU_USED) ...
+
+  _start ();
+}
+
+// ----------------------------------------------------------------------------
