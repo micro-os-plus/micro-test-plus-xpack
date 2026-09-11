4baa3155ca415a303a36d0cff001b2010a5448ad
Liviu Ionescu <ilg@livius.net>
2026-09-11 20:50:58 +0300

    add nucleo-f411re-cubemx-startup

---
+++ tests/platforms/nucleo-f411re-cubemx-startup/README.md
@@ -0,0 +1,21 @@
+# platforms/nucleo-f411re
+
+Note: It cannot be a static library, since it makes extensive use of `weak` symbols.
+
+## OpenOCD invocation
+
+To run the tests, invoke them via OpenOCD:
+
+```sh
+openocd \
+      -c "gdb port disabled" \
+      -c "tcl port disabled" \
+      -c "telnet port disabled" \
+      -f interface/stlink-dap.cfg \
+      -c "adapter speed 5000" \
+      -f target/stm32f4x.cfg \
+      -c "program test.elf verify" \
+      -c "arm semihosting enable" \
+      -c "arm semihosting_cmdline test one two" \
+      -c "reset"
+```
---
+++ tests/platforms/nucleo-f411re-cubemx-startup/include/micro-os-plus/project-config.h
@@ -0,0 +1,31 @@
+/*
+ * DO NOT EDIT! Automatically generated from template file:
+ * build-helper/templates/common/_micro-os-plus/tests/platforms/qemu-cortex-m4f/include/micro-os-plus/project-config-liquid.h
+ * 
+ * This file is part of the µOS++ project (https://micro-os-plus.github.io/).
+ * Copyright (c) 2022-2026 Liviu Ionescu. All rights reserved.
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose is hereby granted, under the terms of the MIT license.
+ *
+ * If a copy of the license was not distributed with this file, it can be
+ * obtained from https://opensource.org/licenses/mit.
+ */
+
+// ----------------------------------------------------------------------------
+
+#ifndef MICRO_OS_PLUS_PROJECT_CONFIG_H_
+#define MICRO_OS_PLUS_PROJECT_CONFIG_H_
+
+// ----------------------------------------------------------------------------
+// Platform configuration definitions.
+// Assembly files should define `#define __ASSEMBLY__ 1` before including
+// this file. If so, this file should define only preprocessor macros.
+
+// ...
+
+// ----------------------------------------------------------------------------
+
+#endif // MICRO_OS_PLUS_PROJECT_CONFIG_H_
+
+// ----------------------------------------------------------------------------
